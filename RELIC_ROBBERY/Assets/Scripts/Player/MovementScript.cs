92a403728eaf486b801e733b2872ad433b43dbbf
Ali-Aref1 <ali14915@gmail.com>
2024-05-26 01:29:35 +0300

    Added running animations

--- RELIC_ROBBERY/Assets/Scripts/Player/MovementScript.cs
+++ RELIC_ROBBERY/Assets/Scripts/Player/MovementScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,14 +10,18 @@ public class MovementScript : MonoBehaviour
     public float moveSpeed = 6f;
     public float sprintMultiplier = 1.5f;
     public float jumpHeight = 3f;
-    public float transitionSmoothing =3f;
+    public float transitionSmoothing = 3f;
     public Transform groundCheck;
     public float gravity = -9.81f;
     public float groundDistance = 0.4f;
     public Animator animator;
 
-    bool sprint(){
-        return Input.GetKeyDown(KeyCode.LeftShift);
+    bool isSprinting = false;
+
+    float sprint()
+    {
+        if (isSprinting) return sprintMultiplier;
+        else return 1;
     }
 
     Vector3 fallVelocity;
@@ -32,27 +37,32 @@ public class MovementScript : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         Vector3 moveDirection = new Vector3(moveX, 0, moveY).normalized;
-        if(moveY>animator.GetFloat("moveY")){
-            animator.SetFloat("moveY",animator.GetFloat("moveY")+transitionSmoothing*Time.deltaTime);
+        if (moveY > animator.GetFloat("moveY"))
+        {
+            animator.SetFloat("moveY", animator.GetFloat("moveY") + transitionSmoothing * Time.deltaTime);
         }
-        if(moveY<animator.GetFloat("moveY")){
-            animator.SetFloat("moveY",animator.GetFloat("moveY")-transitionSmoothing*Time.deltaTime);
+        if (moveY < animator.GetFloat("moveY"))
+        {
+            animator.SetFloat("moveY", animator.GetFloat("moveY") - transitionSmoothing * Time.deltaTime);
         }
-        if(moveX>animator.GetFloat("moveX")){
-            animator.SetFloat("moveX",animator.GetFloat("moveX")+transitionSmoothing*Time.deltaTime);
+        if (moveX > animator.GetFloat("moveX"))
+        {
+            animator.SetFloat("moveX", animator.GetFloat("moveX") + transitionSmoothing * Time.deltaTime);
         }
-        if(moveX<animator.GetFloat("moveX")){
-            animator.SetFloat("moveX",animator.GetFloat("moveX")-transitionSmoothing*Time.deltaTime);
+        if (moveX < animator.GetFloat("moveX"))
+        {
+            animator.SetFloat("moveX", animator.GetFloat("moveX") - transitionSmoothing * Time.deltaTime);
         }
         if (moveDirection.magnitude >= 0.1f) //Check if moving, use later for animations
         {
-            animator.SetBool("walk",true);
+            animator.SetBool("walk", true);
             float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + Camera.eulerAngles.y;
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir * moveSpeed * Time.deltaTime);
+            controller.Move(moveDir * moveSpeed * sprint() * Time.deltaTime);
         }
-        else{
-            animator.SetBool("walk",false);
+        else
+        {
+            animator.SetBool("walk", false);
         }
         //Gravity
         if (isGrounded() && fallVelocity.y < 0)
@@ -65,6 +75,17 @@ public class MovementScript : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             fallVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        //Sprinting
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animator.SetBool("sprint", true);
+            isSprinting = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            animator.SetBool("sprint", false);
+            isSprinting = false;
         }
     }
 }
